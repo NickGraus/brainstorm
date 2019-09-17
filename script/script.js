01e602ac15d80f191b952a45e20dc609d6fba6b9
NickGraus <nick-graus@hotmail.com>
2019-09-17 11:35:24 +0200

    leapjs installed

--- script/script.js
+++ script/script.js
@@ -1,29 +1,52 @@
-var button;
+var btnReset;
+var btnResult;
+var result;
+var output;
 
 function setup() {
-  createCanvas(1080, 500);
+  createCanvas(280, 280);
+  stroke(20);
 
+  btnReset = createButton('reset');
+  btnReset.mousePressed(resetCanvas);
 
-  button = createButton('reset');
-  button.mousePressed(resetCanvas);
+  btnResult = createButton('result');
+  btnResult.mousePressed(result);
 
   background(100);
 }
 
 function resetCanvas() {
-  clear();
   background(100);
+  output.innerHTML = "";
 }
 
-function doodle() {
+function result() {
+  background(100);
+  loadPixels();
+  for(y=0; y<width; y++) {
+    for(x=0; x<width; x++) {
+      var index = (x + y * width)*4;
+      if(pixels[index] != 100) {
+        output.innerHTML = "You didn't draw anything";
+      }
+    }
+  }
+
+  updatePixels();
+
+
+  var resultArray = ["Cat", "Rainbow", "Train", "Bike", "Horse", "Sun", "Cow", "Giraffe", "Tree", "Brain", "Cloud", "Clock", "Plane"];
+  result = resultArray[Math.floor(Math.random() * resultArray.length)];
+  output = document.getElementById("result");
+  output.innerHTML = result;
+}
+
+function draw() {
   stroke(0);
   strokeWeight(10);
   strokeCap(ROUND);
   if (mouseIsPressed === true) {
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
-}
-
-function draw() {
-  doodle();
 }
