c67a66c42e3e6f7d38340151e4f4e1fa10fbb920
NickGraus <nick-graus@hotmail.com>
2019-09-25 11:57:05 +0200

    styling

--- script/script.js
+++ script/script.js
@@ -1,52 +1,97 @@
+var canvas;
 var btnReset;
 var btnResult;
 var result;
 var output;
+var colorRed = 0;
+var colorGreen = 0;
+var colorBlue = 0;
+var resultArray = ["Cat", "Rainbow", "Train", "Bike", "Horse", "Sun", "Cow", "Giraffe", "Tree", "Brain", "Cloud", "Clock", "Plane"];
+
+
 
 function setup() {
-  createCanvas(280, 280);
+  canvas = createCanvas(500, 500);
+  canvas.id("paintCanvas");
+  canvas.class("item2");
   stroke(20);
 
   btnReset = createButton('reset');
   btnReset.mousePressed(resetCanvas);
+  btnReset.id("btnReset");
+  btnReset.class("item5");
 
   btnResult = createButton('result');
-  btnResult.mousePressed(result);
+  btnResult.mousePressed(showResult);
+  btnResult.id("btnResult");
+  btnResult.class("item6");
 
-  background(100);
+  background(255);
+}
+
+
+
+function draw() {
+  stroke(colorRed, colorGreen, colorBlue);
+  strokeWeight(10);
+  strokeCap(ROUND);
+
+  Leap.loop(function(frame) {
+    if (frame.hands.length < 1) {
+      info.innerHTML = "no hands detected";
+    } else {
+      info.innerHTML = "";
+      if (frame.hands[0]) {
+        if (
+          frame.hands[0].thumb.extended == true &&
+          frame.hands[0].indexFinger.extended == true &&
+          frame.hands[0].middleFinger.extended == true &&
+          frame.hands[0].ringFinger.extended == true &&
+          frame.hands[0].pinky.extended == true) {
+
+          colorRed = (frame.hands[0].palmPosition[0] / 255) * 200;
+          colorGreen = (frame.hands[0].palmPosition[1] / 255) * 100;
+          colorBlue = (frame.hands[0].palmPosition[2] / 255) * 200;
+        }
+        if (frame.hands[0].indexFinger) {
+          mouseX = 250 + frame.hands[0].indexFinger.tipPosition[0];
+          mouseY = 500 - frame.hands[0].indexFinger.tipPosition[1];
+          if (
+            frame.hands[0].thumb.extended == false &&
+            frame.hands[0].indexFinger.extended == true &&
+            frame.hands[0].middleFinger.extended == false &&
+            frame.hands[0].ringFinger.extended == false &&
+            frame.hands[0].pinky.extended == false) {
+            if (frame.hands[0].indexFinger.tipPosition[2] < 0) {
+              line(mouseX, mouseY, mouseX, mouseY);
+            }
+
+          }
+        }
+
+      }
+    }
+
+
+  });
 }
 
 function resetCanvas() {
-  background(100);
-  output.innerHTML = "";
+  background(255);
+  output.innerHTML = "The canvas has been reset";
+
+  setTimeout(function() {
+    output.innerHTML = "";
+  }, 2000);
 }
 
-function result() {
-  background(100);
-  loadPixels();
-  for(y=0; y<width; y++) {
-    for(x=0; x<width; x++) {
-      var index = (x + y * width)*4;
-      if(pixels[index] != 100) {
-        output.innerHTML = "You didn't draw anything";
-      }
-    }
-  }
-
-  updatePixels();
 
 
-  var resultArray = ["Cat", "Rainbow", "Train", "Bike", "Horse", "Sun", "Cow", "Giraffe", "Tree", "Brain", "Cloud", "Clock", "Plane"];
+
+
+
+function showResult() {
   result = resultArray[Math.floor(Math.random() * resultArray.length)];
-  output = document.getElementById("result");
+  output = document.getElementById("output");
   output.innerHTML = result;
-}
-
-function draw() {
-  stroke(0);
-  strokeWeight(10);
-  strokeCap(ROUND);
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
 }
